735bacf2b0c80fec1c4023c730f8e2c8b76b5a2c
Dulitha <47850462+dulithaks@users.noreply.github.com>
2021-08-14 10:33:03 +0530

    Add vue compiler

---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3 class="text-center">All Books</h3><br/>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            books: []
+        }
+    }
+}
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,9 @@
+import AllBooks from './components/Posts.vue';
+
+export const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: AllBooks
+    },
+];
